74aede8e9b8de94e459ae54fa3cb727edb1ba3eb
Hyeok <gulww9997@naver.com>
2025-04-04 03:40:56 +0900

    피로도 시스템 구현
    1초마다 증가하는 피로도 시스템 구현

---
+++ Assets/Scripts/Subway/IncreaseTiredness.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using UnityEngine;
+
+public class IncreaseTiredness : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (SubwayManager.Instance.playerState == SubwayManager.PlayerState.SLEEP &&
+            SubwayManager.Instance.currentTired <= SubwayManager.Instance.maxTired)
+        {
+            StartCoroutine(IncreaseTiredPerOneSecond());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator IncreaseTiredPerOneSecond()
+    {
+        while (true)
+        {
+            SubwayManager.Instance.currentTired += SubwayManager.Instance.tiredIncreaseSpeed;
+            yield return new WaitForSeconds(1f);
+
+            if (SubwayManager.Instance.currentTired >= SubwayManager.Instance.maxTired &&
+                SubwayManager.Instance.playerState == SubwayManager.PlayerState.SLEEP)
+            {
+                SubwayManager.Instance.playerState = SubwayManager.PlayerState.DEEPSLEEP;
+                SubwayManager.Instance.currentTired = 0f;
+                // 꿈 속 Scene으로 이동하는 코드
+                break;
+            }
+        }
+    }
+}
