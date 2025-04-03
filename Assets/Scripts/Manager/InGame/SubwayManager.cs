e35b53db5ea41bf9e8d727d0b54e8cb148fb819c
Hyeok <gulww9997@naver.com>
2025-04-04 03:39:54 +0900

    지하철, 꿈 매니저에 변수 선언
    지하철 상태를 관리하는 SubwayManager와 꿈 속 상태를 관리하는 DreamManager에 필요한 변수 선언

--- Assets/Scripts/Manager/InGame/SubwayManager.cs
+++ Assets/Scripts/Manager/InGame/SubwayManager.cs
@@ -2,6 +2,26 @@ using UnityEngine;
 
 public class SubwayManager : Managers<SubwayManager>
 {
+    public enum PlayerState
+    {
+        AWAKE,
+        SLEEP,
+        DEEPSLEEP,
+    }
+
+    public float maxTired;
+    public float currentTired;
+    public float tiredIncreaseSpeed;
+
+    public PlayerState playerState;
+
+    public override void Awake()
+    {
+        playerState = PlayerState.SLEEP;
+        maxTired = 100f;
+        tiredIncreaseSpeed = 2f;
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
