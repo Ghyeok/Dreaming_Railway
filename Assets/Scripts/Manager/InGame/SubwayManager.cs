02b8985e5029176319973e3e071d73d62cd9ee4d
Hyeok <gulww9997@naver.com>
2025-04-04 03:34:59 +0900

    제네릭 매니저 구현, 각종 매니저 생성
    Managers를 제네릭 타입으로 구현함.
    다른 매니저를 생성할 땐, Managers<T>를 상속받아야 함.

---
+++ Assets/Scripts/Manager/InGame/SubwayManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class SubwayManager : Managers<SubwayManager>
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Manager/Managers.cs
@@ -0,0 +1,64 @@
+using System;
+using UnityEngine;
+
+public class Managers<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    public static T Instance { get { Init(); return _instance; } }
+
+    static void Init()
+    {
+        if (_instance == null)
+        {
+            _instance = (T)FindAnyObjectByType(typeof(T));
+
+            if (_instance == null)
+            {
+                CreateInstance();
+            }
+        }
+    }
+
+    static void CreateInstance()
+    {
+        _instance = (T)FindAnyObjectByType(typeof(T));
+
+        if (_instance == null)
+        {
+            GameObject go = new GameObject();
+            go.name = typeof(T).Name;
+            _instance = go.AddComponent<T>();
+            DontDestroyOnLoad(go);
+        }
+    }
+
+    public virtual void Awake()
+    {
+        RemoveDuplicates();
+    }
+
+    private void RemoveDuplicates()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
