631956c22259ae23ab601b53833b6ef86ab30a2b
Kim Dasan <67263093+Plagem@users.noreply.github.com>
2025-04-06 17:42:37 +0900

    feat : UIManager
    UI 생성하는 로직은 UIManager에서 관리합니다
    모든 UI들은 UIRoot 아래 Child로 생성되어 관리됩니다

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -1,16 +1,75 @@
+using System;
 using UnityEngine;
 
 public class UIManager : Managers<UIManager>
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private GameObject _root;
+
+    public static GameObject Root
     {
-        
+        get
+        {
+            if (Instance._root == null)
+            {
+                GameObject root = GameObject.Find("UI_Root");
+                if (root == null)
+                {
+                    root = new GameObject { name = "UI_Root" };
+                }
+
+                Instance._root = root;
+                return Instance._root;
+            }
+            else
+            {
+                return Instance._root;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // 나중에 ExitButton 있는 UI 클래스에서 호출하세요
+    // UIManager.Instance.OnExitButton(); 이런식으로
+    public void OnExitButton()
     {
-        
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    // UI 코드가 한곳에 집중됩니다. 유지보수 편리하고 재사용성도 좋습니다
+    // UIManager.LoadUI(Define.UIType.PauseUI) 이런식으로 쓰시면 됩니다
+    public static GameObject LoadUI(Define.UIType newUIType)
+    {
+        GameObject ui;
+        try
+        {
+            ui = Instantiate(Resources.Load<GameObject>($"Prefabs/UIs/{newUIType}"));
+            ui.transform.parent = Root.transform;
+            return ui;
+        }
+        catch
+        {
+            Debug.LogError($"[Error] UI Load 실패 : Prefabs/UIs/{newUIType} 확인 바람");
+            return null;
+        }
+    }
+    
+    // 이건 String으로 호출하는거
+    public static GameObject LoadUI(string newUIType)
+    {
+        GameObject ui;
+        try
+        {
+            ui = Instantiate(Resources.Load<GameObject>($"Prefabs/UIs/{newUIType}"));
+            ui.transform.parent = Root.transform;
+            return ui;
+        }
+        catch
+        {
+            Debug.LogError($"[Error] UI Load 실패 : Prefabs/UIs/{newUIType} 확인 바람");
+            return null;
+        }
     }
 }
---
+++ Assets/Scripts/Utils/Define.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class Define
+{
+    public enum UIEvent
+    {
+        Click,
+        Drag,
+        DragEnd,
+    }
+
+    public enum Sounds
+    {
+        BGM,
+        VFX,
+        MaxCount
+    }
+
+    public enum BGM
+    {
+        // BGM 목록 작성
+        MaxCount
+    }
+
+    public enum VFX
+    {
+        // VFX 목록 작성
+        MaxCount
+    }
+
+    public enum UIType
+    {
+        // UI 목록 작성
+        MaxCount
+    }
+    
+    // 추가 enum 작성..
+}
