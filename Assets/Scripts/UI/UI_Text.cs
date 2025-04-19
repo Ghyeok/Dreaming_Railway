393a206d9ddd61975f526530d804ec3bab30937f
Hyeok <gulww9997@naver.com>
2025-04-20 03:03:32 +0900

    Feat : 피로도, 환승 UI로 보이게 구현

--- Assets/Scripts/UI/UI_Text.cs
+++ Assets/Scripts/UI/UI_Text.cs
@@ -9,8 +9,12 @@ public class UI_Text : UI_Bind
         DayText,
         TransferText,
         TimeText,
+        TiredText,
         // 필요한 텍스트 추가..
     }
+
+    public TextMeshProUGUI tiredText;
+    public TextMeshProUGUI transferText;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,8 +22,9 @@ public class UI_Text : UI_Bind
         Bind<TextMeshProUGUI>(typeof(Texts));
 
         TextMeshProUGUI dayText = GetText((int)Texts.DayText);
-        TextMeshProUGUI transferText = GetText((int)Texts.TransferText);
+        transferText = GetText((int)Texts.TransferText);
         TextMeshProUGUI timeText = GetText((int)Texts.TimeText);
+        tiredText = GetText((int)Texts.TiredText);
     }
 
     public GameObject BindText(Texts texts)
@@ -30,6 +35,19 @@ public class UI_Text : UI_Bind
     // Update is called once per frame
     void Update()
     {
+        TiredText();
+        TransferText();
+    }
 
+    void TiredText()
+    {
+        TiredManager tiredManager = TiredManager.Instance;
+        tiredText.text = $"{tiredManager.currentTired} / {tiredManager.maxTired}"; 
+    }
+
+    void TransferText()
+    {
+        StationManager stationManager = StationManager.Instance;
+        transferText.text = $"{stationManager.currentStationIdx} / {stationManager.transferStationIdx} Stations";
     }
 }
