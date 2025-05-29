9b8940565c0801028c77a131c189868e58310b88
Seoyun-37 <carrot2202@gmail.com>
2025-05-29 23:49:48 +0900

    배경 적용(좌우루프)

--- Assets/Scripts/InDream/BackgroundSpawn.cs
+++ Assets/Scripts/InDream/BackgroundSpawn.cs
@@ -7,34 +7,38 @@ public class BackgroundSpawn : MonoBehaviour
     public float tileWidth;  // 배경 너비
 
     private float lastSpawnX;
-    private float nextSpawnDistance;
+    private float nextSpawnPoint;
 
     void Start()
     {
+
         lastSpawnX = player.transform.position.x;
-        nextSpawnDistance = tileWidth;
+        nextSpawnPoint = tileWidth;
     }
+
 
     void Update()
     {
         // 플레이어가 일정 거리 이상 이동했는지
-        if (Mathf.Abs(player.transform.position.x) + 15f >= nextSpawnDistance - tileWidth/2f)
+        if (Mathf.Abs(player.transform.position.x) + 15f >= nextSpawnPoint - tileWidth / 2f)
+        //(플레이어 위치)+15가 새로 생기는 배경의 이어지는 부분보다 클때
         {
             Vector3 spawnPosition;
 
             if (player.position.x > lastSpawnX)
             {
                 // 오른쪽에 배경 생성
-                spawnPosition = new Vector3(lastSpawnX + tileWidth, 8f, 5f);
+                spawnPosition = new Vector3(lastSpawnX + tileWidth, 9f, 3f);
             }
             else
             {
                 // 왼쪽에 배경 생성
-                spawnPosition = new Vector3(lastSpawnX - tileWidth, 8f, 5f);
+                spawnPosition = new Vector3(lastSpawnX - tileWidth, 9f, 3f);
             }
 
             Instantiate(Background, spawnPosition, Quaternion.identity, transform);
             lastSpawnX = spawnPosition.x;
+            nextSpawnPoint += tileWidth;
         }
     }
 }
