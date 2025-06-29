b84ab6372b8c77c666ede55bf416ba3e7b8328ca
Hyeok <gulww9997@naver.com>
2025-06-29 16:47:56 +0900

    Feat: 일시정지 -> 조작방법 팝업 구현
    팝업만 구현, 자세한 기획이 필요함

---
+++ Assets/Scripts/UI/Popup/UI_HowToDoPopup.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UI_HowToDoPopup : UI_Popup
+{
+    public enum Buttons
+    {
+        ExitButton,
+
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void Init()
+    {
+        base.Init();
+
+        Bind<Button>(typeof(Buttons));
+
+        GameObject exit = GetButton((int)Buttons.ExitButton).gameObject;
+        AddUIEvent(exit, ExitButtonOnClicked, Define.UIEvent.Click);
+    }
+
+    private void ExitButtonOnClicked(PointerEventData data)
+    {
+        UIManager.Instance.ClosePopupUI(this);
+    }
+}
