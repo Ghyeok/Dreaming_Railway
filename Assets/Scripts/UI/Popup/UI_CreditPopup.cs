eff596188dba71553f5f25245a1daf42359f297d
Hyeok <gulww9997@naver.com>
2025-06-30 18:12:09 +0900

    Feat: 일시정지 -> 환경설정 -> 크레딧 팝업 구현
    기능엔 문제없는데 NullReference 발생하는 오류 존재

---
+++ Assets/Scripts/UI/Popup/UI_CreditPopup.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UI_CreditPopup : UI_Popup
+{
+    public enum Buttons
+    {
+        ExitButton,
+
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void Init()
+    {
+        base.Init();
+        Bind<Button>(typeof(Buttons));
+
+        GameObject exit = GetButton((int)Buttons.ExitButton).gameObject;
+        AddUIEvent(exit, ExitButtonOnClicked, Define.UIEvent.Click);
+    }
+
+    private void ExitButtonOnClicked(PointerEventData data)
+    {
+        UIManager.Instance.ClosePopupUI(this);
+    }
+}
