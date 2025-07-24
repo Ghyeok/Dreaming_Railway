ff306a5d90329da19759441917e813fb0d8696aa
Seoyun-37 <carrot2202@gmail.com>
2025-07-24 13:41:47 +0900

    씬 전환시 효과

---
+++ Assets/Scripts/InDream/WhitePanelSpawn.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class WhitePanelSpawn : MonoBehaviour
+{
+    public Image whitePanel;
+    public float fadeDuration = 0.2f;
+    public float sceneDelay;
+
+
+    void Awake()
+    {
+        whitePanel.color = new Color(1, 1, 1, 0);
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void StartFadeAndLoadScene()
+    {
+        StartCoroutine(FadeInAndLoadScene());
+    }
+    
+    private IEnumerator FadeInAndLoadScene()
+    {
+        yield return null;
+
+        float time = 0f;
+        while (time < fadeDuration)
+        {
+            float alpha = Mathf.Lerp(0f, 1f, time / fadeDuration);
+            whitePanel.color = new Color(1, 1, 1, alpha);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        whitePanel.color = new Color(1, 1, 1, 1);
+
+        yield return new WaitForSeconds(sceneDelay);
+        SceneManager.LoadScene("TestSubwayScene");
+    }
+} 
