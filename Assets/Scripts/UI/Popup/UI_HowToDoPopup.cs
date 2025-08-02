482ace198e72e5532a0b84db7e277cfe9746223d
Hyeok <gulww9997@naver.com>
2025-08-02 18:10:24 +0900

    Feat : 일시정지 -> 도움말 팝업 구현

--- Assets/Scripts/UI/Popup/UI_HowToDoPopup.cs
+++ Assets/Scripts/UI/Popup/UI_HowToDoPopup.cs
@@ -4,9 +4,17 @@ using UnityEngine.UI;
 
 public class UI_HowToDoPopup : UI_Popup
 {
+    public enum Images
+    {
+        HowToDoSubwayImage,
+        HowToDoDreamImage,
+    }
+
     public enum Buttons
     {
         ExitButton,
+        BeforeButton,
+        NextButton,
 
     }
 
@@ -27,13 +35,35 @@ public class UI_HowToDoPopup : UI_Popup
         base.Init();
 
         Bind<Button>(typeof(Buttons));
+        Bind<Image>(typeof(Images));
 
         GameObject exit = GetButton((int)Buttons.ExitButton).gameObject;
         AddUIEvent(exit, ExitButtonOnClicked, Define.UIEvent.Click);
+
+        GameObject before = GetButton((int)Buttons.BeforeButton).gameObject;
+        AddUIEvent(before, BeforeButtonOnClicked, Define.UIEvent.Click);
+
+        GameObject next = GetButton((int)Buttons.NextButton).gameObject;
+        AddUIEvent(next, NextButtonOnClicked, Define.UIEvent.Click);
+
+        GetImage((int)Images.HowToDoDreamImage).gameObject.SetActive(false);
+        GetImage((int)Images.HowToDoSubwayImage).gameObject.SetActive(true);
     }
 
     private void ExitButtonOnClicked(PointerEventData data)
     {
         UIManager.Instance.ClosePopupUI(this);
+    }
+
+    private void BeforeButtonOnClicked(PointerEventData data)
+    {
+        GetImage((int)Images.HowToDoDreamImage).gameObject.SetActive(false);
+        GetImage((int)Images.HowToDoDreamImage - 1).gameObject.SetActive(true);
+    }
+
+    private void NextButtonOnClicked(PointerEventData data)
+    {
+        GetImage((int)Images.HowToDoSubwayImage).gameObject.SetActive(false);
+        GetImage((int)Images.HowToDoSubwayImage + 1).gameObject.SetActive(true);
     }
 }
