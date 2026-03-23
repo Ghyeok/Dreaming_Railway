1e8920a83d160701fff67d20472781373059e8c1
Ghyeok <gulww9997@naver.com>
2026-03-23 18:29:08 +0900

    rename: SubwayLineData

--- Assets/Scripts/Content/SubwayLine/SubwayLineData.cs
+++ Assets/Scripts/Content/SubwayLine/SubwayLineData.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class LineData
+public class SubwayLineData
 {
     public List<StationData> stations = new List<StationData>(); // 20개역을 넘을 수 없으므로 넉넉하게
     public int transferIdx = -1;
