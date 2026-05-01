1da6b57dc43447328036828f61969f07883c504d
Ghyeok <gulww9997@naver.com>
2026-05-02 02:57:29 +0900

    refactor: StageSelectScene

--- Assets/Scripts/Content/UI/Scene/UI_StageSelectScene.cs
+++ Assets/Scripts/Content/UI/Scene/UI_StageSelectScene.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UI_StageSelectScene : UI_Scene
 {
+    private const string KEY_SUBWAY_POS_X = "SubwayPosX";
+
     private SubwayMiniMove subwayMiniMove;
+    private Animator _animator;
 
     [SerializeField] private Sprite stageLock;
     [SerializeField] private Sprite stageUnlock;
@@ -47,38 +49,26 @@ public class UI_StageSelectScene : UI_Scene
 
         subwayMiniMove = GetImage((int)Images.SubwayMini).GetComponent<SubwayMiniMove>();
 
-        GameObject stage0 = GetButton((int)Buttons.Stage0).gameObject;
-        AddUIEvent(stage0, Stage0ButtonOnClicked);
-
-        GameObject stage1 = GetButton((int)Buttons.Stage1).gameObject;
-        AddUIEvent(stage1, Stage1ButtonOnClicked);
-
-        GameObject stage2 = GetButton((int)Buttons.Stage2).gameObject;
-        AddUIEvent(stage2, Stage2ButtonOnClicked);
-
-        GameObject stage3 = GetButton((int)Buttons.Stage3).gameObject;
-        AddUIEvent(stage3, Stage3ButtonOnClicked);
-
-        GameObject stage4 = GetButton((int)Buttons.Stage4).gameObject;
-        AddUIEvent(stage4, Stage4ButtonOnClicked);
-
-        GameObject stage5 = GetButton((int)Buttons.Stage5).gameObject;
-        AddUIEvent(stage5, Stage5ButtonOnClicked);
-
-        GameObject back = GetButton((int)Buttons.BackButton).gameObject;
-        AddUIEvent(back, BackButtonOnClicked);
+        AddUIEvent(GetButton((int)Buttons.Stage0).gameObject, data => OnStageClicked(0, subwayMiniMove.position0));
+        AddUIEvent(GetButton((int)Buttons.Stage1).gameObject, data => OnStageClicked(1, subwayMiniMove.position1));
+        AddUIEvent(GetButton((int)Buttons.Stage2).gameObject, data => OnStageClicked(2, subwayMiniMove.position2));
+        AddUIEvent(GetButton((int)Buttons.Stage3).gameObject, data => OnStageClicked(3, subwayMiniMove.position3));
+        AddUIEvent(GetButton((int)Buttons.Stage4).gameObject, data => OnStageClicked(4, subwayMiniMove.position4));
+        AddUIEvent(GetButton((int)Buttons.Stage5).gameObject, data => OnStageClicked(5, subwayMiniMove.position5));
+        AddUIEvent(GetButton((int)Buttons.BackButton).gameObject, BackButtonOnClicked);
 
         SetActiveTrueButtons();
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Init();
+        _animator = GetComponentInChildren<Animator>();
+
         UI_FadeBlackPanel fadePanel = UIManager.Instance.ShowPopupUI<UI_FadeBlackPanel>();
         fadePanel.StartFadeIn(0.3f);
 
-        LoadSubwayPosition(); //저장된 게임 정보
+        LoadSubwayPosition();
         LoadStageLock();
     }
 
@@ -92,181 +82,83 @@ public class UI_StageSelectScene : UI_Scene
         Get<GameObject>((int)GameObjects.ButtonRoot).gameObject.SetActive(true);
     }
 
-    IEnumerator EnterToSubway(RectTransform newPositionTransform)
+    private void OnStageClicked(int day, RectTransform position)
     {
-        Vector2 newPosition = newPositionTransform.anchoredPosition; //벡터 형태로 수정
-
-        float priorPosX = PlayerPrefs.HasKey("SubwayPosX") ? PlayerPrefs.GetFloat("SubwayPosX") : subwayMiniMove.position0.anchoredPosition.x;
-
-        if (Mathf.Abs(priorPosX - newPosition.x) > 10f) //저장된 위치와 새로 입력된 위치가 다르다면
+        if (day == 0)
         {
-            subwayMiniMove.MoveToPosition(newPositionTransform);
+            GameManager.Instance.ChangeGameMode(GameMode.Tutorial);
+        }
+        else
+        {
+            GameManager.Instance.ChangeGameMode(GameMode.NormalMode);
+            ScriptManager.Instance.isStart = true;
+        }
+        GameManager.Instance.StartDay(day);
+        StartCoroutine(EnterToSubway(position));
+    }
 
-            yield return new WaitForSeconds(3f); // 미니 버스 움직이는 시간
+    private IEnumerator EnterToSubway(RectTransform targetPosition)
+    {
+        float defaultX = subwayMiniMove.position0.anchoredPosition.x;
+        float priorPosX = PlayerPrefs.HasKey(KEY_SUBWAY_POS_X)
+            ? PlayerPrefs.GetFloat(KEY_SUBWAY_POS_X)
+            : defaultX;
+
+        if (Mathf.Abs(priorPosX - targetPosition.anchoredPosition.x) > 10f)
+        {
+            subwayMiniMove.MoveToPosition(targetPosition);
+            yield return new WaitForSeconds(1.5f); // 스테이지 선택 후 대기 시간
         }
 
-        SaveSubwayPosition(newPosition);
+        SaveSubwayPosition(targetPosition.anchoredPosition);
 
-        Animator anim = GetComponentInChildren<Animator>();
-        anim.SetTrigger("ButtonClicked");
+        _animator.SetTrigger("ButtonClicked");
         SetActiveFalseButtons();
 
-        yield return new WaitForSeconds(6.4f); //애니매이션 연출 시간
+        yield return new WaitForSeconds(7f); // 애니메이션 재생 대기 시간
 
-        SceneManager.LoadScene("SubwayScene");
+        SceneTransitionManager.Instance.GoToSubway();
     }
 
     private void BackButtonOnClicked(PointerEventData data)
     {
-        StartCoroutine(FadeAndLoadScene("MainScene"));
-    }
-
-    private void Stage0ButtonOnClicked(PointerEventData data)
-    {
-        GameManager.Instance.ChangeGameMode(GameMode.Tutorial);
-        GameManager.Instance.StartDay(0);
-
-        StartCoroutine(EnterToSubway(subwayMiniMove.position0)); //위치 정보 전달
-    }
-
-    private void Stage1ButtonOnClicked(PointerEventData data)
-    {
-        GameManager.Instance.ChangeGameMode(GameMode.NormalMode);
-        GameManager.Instance.StartDay(1);
-
-        ScriptManager.Instance.isStart = true;
-        StartCoroutine(EnterToSubway(subwayMiniMove.position1)); //위치 정보 전달
-    }
-
-    private void Stage2ButtonOnClicked(PointerEventData data)
-    {
-        GameManager.Instance.ChangeGameMode(GameMode.NormalMode);
-        GameManager.Instance.StartDay(2);
-
-        ScriptManager.Instance.isStart = true;
-        StartCoroutine(EnterToSubway(subwayMiniMove.position2)); //위치 정보 전달
-    }
-
-    private void Stage3ButtonOnClicked(PointerEventData data)
-    {
-        GameManager.Instance.ChangeGameMode(GameMode.NormalMode);
-        GameManager.Instance.StartDay(3);
-
-        ScriptManager.Instance.isStart = true;
-        StartCoroutine(EnterToSubway(subwayMiniMove.position3)); //위치 정보 전달
-    }
-
-    private void Stage4ButtonOnClicked(PointerEventData data)
-    {
-        GameManager.Instance.ChangeGameMode(GameMode.NormalMode);
-        GameManager.Instance.StartDay(4);
-
-        ScriptManager.Instance.isStart = true;
-        StartCoroutine(EnterToSubway(subwayMiniMove.position4)); //위치 정보 전달
-    }
-
-    private void Stage5ButtonOnClicked(PointerEventData data)
-    {
-        GameManager.Instance.ChangeGameMode(GameMode.NormalMode);
-        GameManager.Instance.StartDay(5);
-
-        ScriptManager.Instance.isStart = true;
-        StartCoroutine(EnterToSubway(subwayMiniMove.position5)); //위치 정보 전달
-    }
-
-    IEnumerator FadeAndLoadScene(string sceneName)
-    {
-        UI_FadeBlackPanel fadePanel = UIManager.Instance.ShowPopupUI<UI_FadeBlackPanel>();
-
-        fadePanel.Init();
-        yield return fadePanel.Fade(0f, 1f, 0.3f); //페이드아웃
-
-        // 비동기 씬 로드
-        AsyncOperation async = SceneManager.LoadSceneAsync(sceneName);
-        async.allowSceneActivation = false; // 페이드아웃 끝난 뒤 활성화
-
-        //씬이 거의 다 로드될 때까지 대기
-        while (async.progress < 0.9f)
-        {
-            yield return null;
-        }
-        async.allowSceneActivation = true;
-
-
-        yield return null;
+        SceneTransitionManager.Instance.GoToScene(SceneName.Main, FadeType.Black);
     }
 
     private void SaveSubwayPosition(Vector2 position)
     {
-        PlayerPrefs.SetFloat("SubwayPosX", position.x); //x값만 저장
+        PlayerPrefs.SetFloat(KEY_SUBWAY_POS_X, position.x);
         PlayerPrefs.Save();
     }
 
     private void LoadSubwayPosition()
     {
-        // 저장된 위치 정보가 있는지 확인
-        if (PlayerPrefs.HasKey("SubwayPosX"))
-        {
-            float x = PlayerPrefs.GetFloat("SubwayPosX");
-            Vector2 savedPosition = new Vector2(x, 464f);
-
-            subwayMiniMove.transform.GetComponent<RectTransform>().anchoredPosition = savedPosition;
-        }
-        else
-        {
-            subwayMiniMove.transform.GetComponent<RectTransform>().anchoredPosition = new Vector2(subwayMiniMove.position0.anchoredPosition.x, 464f);
-        }
+        float defaultX = subwayMiniMove.position0.anchoredPosition.x;
+        float x = PlayerPrefs.GetFloat(KEY_SUBWAY_POS_X, defaultX);
+        subwayMiniMove.GetComponent<RectTransform>().anchoredPosition = new Vector2(x, 464f);
     }
 
     private void LoadStageLock()
     {
-        // 마지막으로 클리어한 스테이지의 인덱스 (아무 것도 안 깼으면 -1)
         int m = SaveManager.Instance.LoadMaxClearDay();
 
-        // 1) m+1..5 잠금
-        for (int i = m + 1; i <= 5; i++)
-        {
-            var btn = GetButton((int)Buttons.Stage0 + i);
-            var img = btn.GetComponent<Image>();
-            var b = btn.GetComponent<Button>();
-            var cg = Util.GetOrAddComponent<CanvasGroup>(btn.gameObject);
+        for (int i = 0; i <= 5; i++)
+            SetStageButtonState(i, i <= m + 1);
 
-            img.sprite = stageLock;
-            img.raycastTarget = false;
-            b.interactable = false;
-            cg.blocksRaycasts = false;
-            cg.interactable = false;
-        }
+        for (int i = 0; i <= 4; i++)
+            GetImage((int)Images.UnderBar + i).color = i <= m ? Color.white : new Color(0.65f, 0.65f, 0.65f);
+    }
 
-        // 언더바 잠금 색상: m+1..4
-        for (int i = m + 1; i <= 4; i++)
-        {
-            GetImage((int)Images.UnderBar + i).GetComponent<Image>().color = new Color(0.65f, 0.65f, 0.65f);
-        }
+    private void SetStageButtonState(int stageIndex, bool unlocked)
+    {
+        var btn = GetButton((int)Buttons.Stage0 + stageIndex);
+        var img = btn.GetComponent<Image>();
+        var cg = Util.GetOrAddComponent<CanvasGroup>(btn.gameObject);
 
-        // 2) 0..m 잠금해제
-        for (int i = 0; i <= m + 1; i++)
-        {
-            if (i == 6) return;
-
-            var btn = GetButton((int)Buttons.Stage0 + i);
-            var img = btn.GetComponent<Image>();
-            var b = btn.GetComponent<Button>();
-            var cg = Util.GetOrAddComponent<CanvasGroup>(btn.gameObject);
-
-            img.sprite = stageUnlock;
-            img.raycastTarget = true;
-            b.interactable = true;
-            cg.blocksRaycasts = true;
-            cg.interactable = true;
-        }
-
-        // 언더바 해제 색상: 0..m (언더바는 0..4까지만 존재하니 한 번 더 가드)
-        for (int i = 0; i <= m + 1; i++)
-        {
-            if (i > 4) return;
-
-            GetImage((int)Images.UnderBar + i).GetComponent<Image>().color = new Color(1f, 1f, 1f);
-        }
+        img.sprite = unlocked ? stageUnlock : stageLock;
+        img.raycastTarget = unlocked;
+        btn.interactable = unlocked;
+        cg.blocksRaycasts = unlocked;
+        cg.interactable = unlocked;
     }
 }
