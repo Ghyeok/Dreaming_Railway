9f4d60125e1fa1aa112cadc82514071efe9d503e
Ghyeok <gulww9997@naver.com>
2026-05-02 02:30:55 +0900

    feat: SceneTransitionManager
    씬 전환 진입점 통일

---
+++ Assets/Scripts/Core/Managers/SceneTransitionManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum FadeType { None, Black }
+
+public class SceneTransitionManager : SingletonManagers<SceneTransitionManager>, IManager
+{
+    private bool _isTransitioning;
+
+    public void Init() { }
+
+    public void GoToMain() => GoToScene(SceneName.Main);
+    public void GoToStageSelect() => GoToScene(SceneName.StageSelect);
+    public void GoToSubway() => GoToScene(SceneName.Subway);
+    public void GoToDream() => GoToScene(SceneName.Dream);
+
+    public void GoToScene(string sceneName, FadeType fadeType = FadeType.None)
+    {
+        if (_isTransitioning) return;
+        StartCoroutine(TransitionRoutine(sceneName, fadeType));
+    }
+
+    private IEnumerator TransitionRoutine(string sceneName, FadeType fadeType)
+    {
+        _isTransitioning = true;
+
+        if (fadeType == FadeType.Black)
+        {
+            UI_FadeBlackPanel fadePanel = UIManager.Instance.ShowPopupUI<UI_FadeBlackPanel>();
+            fadePanel.Init();
+            yield return fadePanel.Fade(0f, 1f, 0.3f);
+        }
+
+        AsyncOperation async = SceneManager.LoadSceneAsync(sceneName);
+        async.allowSceneActivation = false;
+
+        while (async.progress < 0.9f)
+            yield return null;
+
+        async.allowSceneActivation = true;
+        yield return null;
+
+        _isTransitioning = false;
+    }
+}
