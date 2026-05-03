35563c1492b05cf802cf50099a8e9813ebd395e5
Ghyeok <gulww9997@naver.com>
2026-05-04 01:34:38 +0900

    refactor: SubwayData

--- Assets/Scripts/Content/Subway/LineData.cs
+++ Assets/Scripts/Content/Subway/LineData.cs
@@ -2,9 +2,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class SubwayLineData
+public class LineData
 {
     public List<StationData> stations = new List<StationData>(); // 20개역을 넘을 수 없으므로 넉넉하게
-    public int transferIdx = -1;
-    public bool hasDestination = false;
+    public int transferIdx = -1; // 환승역 인덱스
+    public bool hasDestination = false; // Line에 도착역이 있는지
 }
